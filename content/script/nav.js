54160359401eee4ebeb796c1615799c68da4ab0c
songht7 <leon.ting@okkuaixiu.com>
2019-11-04 21:38:15 +0800

    xxx

--- content/script/nav.js
+++ content/script/nav.js
@@ -29,12 +29,22 @@ class MobileNav extends React.Component {
     render() {
         return (
             <ul>
-                <li className="tri_delay_2"><a href="introduction.html">介绍</a></li>
+                <li className="tri_delay_1">
+                    <a href="index.html">作品</a>
+                    <ul className="nav_sub_m">
+                        <li><a href="index.html?ctg=1"><span>私宅、样板房、别墅</span></a></li>
+                        <li><a href="index.html?ctg=2"><span>会所、售楼处</span></a></li>
+                        <li><a href="index.html?ctg=3"><span>酒店</span></a></li>
+                        <li><a href="index.html?ctg=4"><span>办公</span></a></li>
+                        <li><a href="index.html?ctg=5"><span>商业空间</span></a></li>
+                    </ul>
+                </li>
+                <li className="tri_delay_2"><a href="introduction.html">简介</a></li>
                 {/* <li className="tri_delay_2"><a href="award.html">AWARDS</a></li>
                 <li className="tri_delay_3"><a href="news.html">NEWS</a></li>
                 <li className="tri_delay_3"><a href="team.html">TEAM</a></li>*/}
-                <li className="tri_delay_3"><a href="contact.html">Contact Us</a></li>
-                <li className="tri_delay_4"><a href="job.html">JOIN US</a></li>
+                <li className="tri_delay_4"><a href="job.html">招聘</a></li>
+                <li className="tri_delay_3"><a href="contact.html">联系我们</a></li>
             </ul>
         );
     }
@@ -56,7 +66,7 @@ class PCNav extends React.Component {
                 </li>
                 <li><a href="introduction.html"><span>简介</span></a></li>
                 <li><a href="job.html"><span>招聘</span></a></li>
-                <li><a href="contact.html"><span>联系</span></a></li>
+                <li><a href="contact.html"><span>联系我们</span></a></li>
             </ul>
         );
     }
