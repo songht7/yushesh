5e9576cf590d95b741533d3434f2d7035be22389
Leon Song <leon.ting@okkuaixiu.com>
2019-11-07 15:40:40 +0800

    Xxxx

--- content/script/lib/top-header.js
+++
@@ -1,3 +0,0 @@
-<div id="new_footer_container" class="pc_block">
-	<p>版权所有 ©御舍 沪ICP备19029924号-1</p>
-</div>
--- content/script/product.json.js
+++ content/script/product.json.js
@@ -16,7 +16,7 @@ var proList = [{
     "area": "上海市長寧區長寧來福士辦公樓1A22F",
     "construction": "御舍（上海）建设工程有限公司",
     "area": "1000㎡",
-    "style":"",
+    "style": "",
     "txtPlace": 7,
     "textRlign": "right",
     "showType": "type_a",
@@ -56,19 +56,19 @@ var proList = [{
         "left": "440",
         "top": "690"
     }],
-    "designConcept":"以象形文字中品字的形字以馬的蹄印演化而來，而馬的精神正是品優，品質、品格，代表著公司的三位股東精神，及企業形象",
+    "designConcept": "以象形文字中品字的形字以馬的蹄印演化而來，而馬的精神正是品優，品質、品格，代表著公司的三位股東精神，及企業形象",
     "content": "",
-    "dtlImgs":["1.jpg","2.jpg","3.png","4.jpg","5.jpg","6.jpg","7.png","8.png"]
+    "dtlImgs": ["1.jpg", "2.jpg", "3.png", "4.jpg", "5.jpg", "6.jpg", "7.png", "8.png"]
 }, {
     "id": 14,
     "ctg": 2,
-    "title": "22222",
+    "title": "芙蕖茶苑会所",
     "design": "李宏升",
     "designCom": "御舍设计",
-    "location": "中国上海",
+    "location": "江西九江",
     "construction": "御舍（上海）建设工程有限公司",
-    "area": "1000㎡",
-    "txtPlace": 7,
+    "area": "1500㎡",
+    "txtPlace": 5,
     "textRlign": "right",
     "showType": "type_a",
     "cover": [{
@@ -78,83 +78,52 @@ var proList = [{
         "top": "0"
     }, {
         "url": "c2.jpg",
-        "size": "420x210",
+        "size": "860x210",
         "left": "440",
         "top": "0"
     }, {
         "url": "c3.jpg",
-        "size": "420x670",
-        "left": "880",
-        "top": "0"
-    }, {
-        "url": "c4.jpg",
-        "size": "420x210",
+        "size": "860x210",
         "left": "440",
         "top": "230"
     }, {
-        "url": "c5.jpg",
+        "url": "c4.jpg",
         "size": "860x210",
         "left": "0",
         "top": "460"
     }, {
-        "url": "c6.jpg",
-        "size": "420x210",
+        "url": "c5.jpg",
+        "size": "860x210",
         "left": "0",
         "top": "690"
     }, {
-        "url": "c7.jpg",
-        "size": "860x210",
-        "left": "440",
-        "top": "690"
-    }]
+        "url": "c6.jpg",
+        "size": "420x440",
+        "left": "880",
+        "top": "460"
+    }],
+    "designConcept": "",
+    "content": "",
+    "dtlImgs": ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"]
 }, {
-    "id": 15,
-    "ctg": 2,
-    "title": "3333",
+    "id": 2,
+    "ctg": 3,
+    "title": "信华酒店",
     "design": "李宏升",
     "designCom": "御舍设计",
-    "location": "中国上海",
+    "softDesign": "陈婷婷",
+    "partDesign": "王景前/刘坤",
+    "location": "江西九江",
     "construction": "御舍（上海）建设工程有限公司",
-    "area": "1000㎡",
+    "area": "3000㎡",
     "txtPlace": 7,
-    "textRlign": "right",
-    "showType": "type_a",
-    "cover": [{
-        "url": "c1.jpg",
-        "size": "420x440",
-        "left": "0",
-        "top": "0"
-    }, {
-        "url": "c2.jpg",
-        "size": "420x210",
-        "left": "440",
-        "top": "0"
-    }, {
-        "url": "c3.jpg",
-        "size": "420x670",
-        "left": "880",
-        "top": "0"
-    }, {
-        "url": "c4.jpg",
-        "size": "420x210",
-        "left": "440",
-        "top": "230"
-    }, {
-        "url": "c5.jpg",
-        "size": "860x210",
-        "left": "0",
-        "top": "460"
-    }, {
-        "url": "c6.jpg",
-        "size": "420x210",
-        "left": "0",
-        "top": "690"
-    }, {
-        "url": "c7.jpg",
-        "size": "860x210",
-        "left": "440",
-        "top": "690"
-    }]
+    "textRlign": "",
+    "showType": "single",
+    "cover": "c1.jpg",
+    "designConcept": "",
+    "content": "该项目坐落于有人杰地灵之称的浔阳，酒店以婚礼及宴席为主，寻佳作、觅 佳偶，以此为题展开设计。将浪漫之形高贵之气印染至所目及之处，形神兼 备的传达质感与气氛。",
+    "dtlImgType": "full",
+    "dtlImgs": ["2.jpg", "3.jpg", "4.jpg", "5.png", "6.jpg", "7.png", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg", "15.jpg", "16.jpg", "17.jpg", "18.jpg", "19.jpg", "20.jpg", "21.jpg", "22.jpg", "23.jpg", "24.jpg", "25.jpg", "26.jpg", "27.jpg", "28.jpg", "29.jpg", "30.jpg", "21.jpg"]
 }, {
     "id": 16,
     "ctg": 3,
