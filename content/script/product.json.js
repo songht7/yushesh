b4aa5216d028780c8c17df0ebda68e30c4bb9001
Leon Song <leon.ting@okkuaixiu.com>
2019-11-08 11:17:35 +0800

    Xxxx

--- content/script/product.json.js
+++ content/script/product.json.js
@@ -252,8 +252,8 @@ var proList = [{
     "location": "浙江桐乡",
     "construction": "御舍（上海）建设工程有限公司",
     "area": "1300㎡",
-    "txtPlace": 5,
-    "textRlign": "right",
+    "txtPlace": 4,
+    "textRlign": "",
     "showType": "type_a",
     "cover": [{
         "url": "c1.jpg",
@@ -363,4 +363,52 @@ var proList = [{
     "content": "",
     "dtlImgType": "full",
     "dtlImgs": ["1.png", "2.png", "3.png"]
+}, {
+    "id": 20,
+    "ctg": 3,
+    "ctgName":"酒店",
+    "title": "上海康桥精品酒店",
+    "design": "李宏升",
+    "designCom": "御舍设计",
+    "location": "中国上海",
+    "construction": "御舍（上海）建设工程有限公司",
+    "area": "20000㎡",
+    "txtPlace": 6,
+    "textRlign": "right",
+    "showType": "type_a",
+    "cover": [{
+        "url": "c1.jpg",
+        "size": "420x440",
+        "left": "0",
+        "top": "0"
+    }, {
+        "url": "c2.jpg",
+        "size": "420x440",
+        "left": "440",
+        "top": "0"
+    }, {
+        "url": "c3.jpg",
+        "size": "420x440",
+        "left": "880",
+        "top": "0"
+    }, {
+        "url": "c4.jpg",
+        "size": "420x440",
+        "left": "0",
+        "top": "460"
+    }, {
+        "url": "c5.jpg",
+        "size": "420x440",
+        "left": "440",
+        "top": "460"
+    }, {
+        "url": "c6.jpg",
+        "size": "420x440",
+        "left": "880",
+        "top": "460"
+    }],
+    "designConcept": "",
+    "content": "",
+    "dtlImgType": "full",
+    "dtlImgs": ["1.png","2.png", "3.png"]
 }];
