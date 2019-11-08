54d9f8edbd018ef47aaf1cbae94337753c67b02e
Leon Song <leon.ting@okkuaixiu.com>
2019-11-08 11:05:34 +0800

    Xxxx

--- content/script/product.json.js
+++ content/script/product.json.js
@@ -199,7 +199,7 @@ var proList = [{
     "location": "中国上海",
     "construction": "御舍（上海）建设工程有限公司",
     "area": "600㎡",
-    "txtPlace": 5,
+    "txtPlace": 7,
     "textRlign": "right",
     "showType": "type_a",
     "cover": [{
@@ -262,12 +262,12 @@ var proList = [{
         "top": "0"
     }, {
         "url": "c2.jpg",
-        "size": "860x4210",
+        "size": "860x210",
         "left": "440",
         "top": "0"
     }, {
         "url": "c3.jpg",
-        "size": "860x4210",
+        "size": "860x210",
         "left": "440",
         "top": "230"
     }, {
@@ -277,12 +277,12 @@ var proList = [{
         "top": "460"
     }, {
         "url": "c5.jpg",
-        "size": "860x4210",
+        "size": "860x210",
         "left": "440",
         "top": "460"
     }, {
         "url": "c6.jpg",
-        "size": "860x4210",
+        "size": "860x210",
         "left": "440",
         "top": "690"
     }],
@@ -290,4 +290,77 @@ var proList = [{
     "content": "",
     "dtlImgType": "full",
     "dtlImgs": ["1.png","2.png", "3.png"]
+}, {
+    "id": 18,
+    "ctg": 3,
+    "ctgName":"酒店",
+    "title": "山东福乐蒙酒店",
+    "design": "李宏升",
+    "designCom": "御舍设计",
+    "location": "山东",
+    "construction": "御舍（上海）建设工程有限公司",
+    "area": "25000㎡",
+    "txtPlace": 5,
+    "textRlign": "right",
+    "showType": "type_a",
+    "cover": [{
+        "url": "c1.jpg",
+        "size": "420x210",
+        "left": "0",
+        "top": "0"
+    }, {
+        "url": "c2.jpg",
+        "size": "860x210",
+        "left": "440",
+        "top": "0"
+    }, {
+        "url": "c3.jpg",
+        "size": "420x440",
+        "left": "0",
+        "top": "230"
+    }, {
+        "url": "c4.jpg",
+        "size": "420x440",
+        "left": "440",
+        "top": "230"
+    }, {
+        "url": "c5.jpg",
+        "size": "420x440",
+        "left": "880",
+        "top": "230"
+    }, {
+        "url": "c6.jpg",
+        "size": "860x210",
+        "left": "0",
+        "top": "690"
+    }, {
+        "url": "c7.jpg",
+        "size": "420x210",
+        "left": "880",
+        "top": "690"
+    }],
+    "designConcept": "",
+    "content": "",
+    "dtlImgType": "full",
+    "dtlImgs": ["1.png","2.png", "3.png"]
+}, {
+    "id": 19,
+    "ctg": 3,
+    "ctgName":"酒店",
+    "title": "上海敏帝豪酒店",
+    "design": "李宏升",
+    "designCom": "御舍设计",
+    "softDesign": "",
+    "partDesign": "",
+    "location": "中国上海",
+    "construction": "御舍（上海）建设工程有限公司",
+    "area": "20000㎡",
+    "txtPlace": 1,
+    "textRlign": "",
+    "showType": "single",
+    "cover": "c1.png",
+    "designConcept": "",
+    "content": "",
+    "dtlImgType": "full",
+    "dtlImgs": ["1.png", "2.png", "3.png"]
 }];
