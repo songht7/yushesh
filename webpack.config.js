454f17b0fbcac8f863888f6eff3f5f9cbd88babc
Leon Song <leon.ting@okkuaixiu.com>
2019-11-08 17:57:14 +0800

    Xxxx

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
-
 module.exports = {
     mode: 'production',
     entry: {
-    	bundle:['./content/script/lib/vue-components.js','./content/script/lib/vue-data.js']
+        bundle: [
+            './content/script/lib/vue-components.js',
+            './content/script/lib/vue-data.js'
+        ]
     },
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, './content/script/lib/'),
         filename: '[name].js'
     }
 };
